67d1e772572fa04494b0883f418ec5f140f0c625
Youlserf <youlserf.cardenas@tecsup.edu.pe>
2022-06-01 11:40:02 -0500

    crud, absence iiner join

--- biblioteca/serializers.py
+++ biblioteca/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
 
-from .models import Prestamo
+from .models import Prestamo, Libro, Usuario
 
 class PrestamoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prestamo
         fields = ['id', 'usuario', 'libro', 'f_prestamo', 'f_devolucion']
+
+class InnerJoinSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prestamo
+        fields = ['id', 'usuario', 'libro', 'f_prestamo', 'f_devolucion', ""]
+        
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Libro
+        fields = ['codigo', 'titulo']
+        
+class LibroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['nombre']
